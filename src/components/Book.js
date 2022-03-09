46a2ed0527b8e43e7093a4f38fb96c66d82654c0
Sumair <sumairqaisar992@gmail.com>
2022-03-09 16:14:02 +0500

    Add delete functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+// import your Action Creators
 import PropTypes from 'prop-types';
+import { removeBook } from '../redux/books/books';
 
-const Book = ({ title, genre, author }) => (
-  <div>
-    <div className="genre">{genre}</div>
-    <div className="title">{title}</div>
-    <div className="author">{author}</div>
-    <button type="button">Delete</button>
-  </div>
-);
+const Book = ({
+  title, author, id,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = (e) => {
+    dispatch(removeBook(e.target.id));
+  };
+  return (
+    <div>
+      <div className="title">{title}</div>
+      <div className="author">{author}</div>
+      <button type="button" id={id} onClick={handleDelete}>Delete</button>
+    </div>
+  );
+};
 
 Book.propTypes = {
   title: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default Book;
