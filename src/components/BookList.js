b8200a0815137ab57221d66bca1d88a72fd59722
Sumair <sumairqaisar992@gmail.com>
2022-03-09 16:15:20 +0500

    Access books stored in redux state

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,30 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import InputBook from './InputBook';
 
-const dummyData = [{
-  title: 'Dunes',
-  author: 'Frank Herbert',
-  genre: 'Action',
-  id: 1,
-},
-{
-  title: 'Alchemist',
-  author: 'Paulo Cohelo',
-  genre: 'Fiction',
-  id: 2,
-},
-];
-const BookList = () => (
-  <div>
-    {dummyData.map((book) => (
-      <Book title={book.title} genre={book.genre} author={book.author} key={book.id} />
-    ))}
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+  return (
     <div>
-      <h2>Add New Book</h2>
-      <InputBook />
+      {books.map((book) => (
+        <Book
+          title={book.title}
+          author={book.author}
+          key={book.id}
+          id={book.id}
+        />
+      ))}
+      <div>
+        <h2>Add New Book</h2>
+        <InputBook />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default BookList;
