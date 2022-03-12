e52b97fdd0cfc4b82a98125a779afba4303ec4cb
Sumair <sumairqaisar992@gmail.com>
2022-03-12 13:56:42 +0500

    Add styles

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './redux/configureStore';
-import './index.css';
+import './index.scss';
 import App from './App';
 
 ReactDOM.render(
