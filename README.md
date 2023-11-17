044785aa2e41af57841c195e61ffccd1d789990e
sumairq <sumairqaisar992@gmail.com>
2023-11-17 21:11:29 +0500

    Update live link in readme

--- README.md
+++ README.md
@@ -60,7 +60,7 @@
 
 ## 🚀 Live Demo <a name="live-demo"></a>
 
-- [Live Demo Link](https://google.com)
+- [Live Demo Link](https://sumairq.github.io/readings-bookstore/)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { ImUser } from 'react-icons/im';
+import { FcIdea } from 'react-icons/fc';
 
 const Header = () => (
   <header>
-    <h1>Bookstore CMS</h1>
+    <FcIdea size={50} />
+    <h1>Readings</h1>
     <nav>
       <Link to="/">Books</Link>
       <Link to="/categories">Categories</Link>
